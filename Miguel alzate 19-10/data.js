afa9f2a508145ace7b2071e2fc1b921316ba0d83
MigueChechoooo <127440243+MiguelChechooo@users.noreply.github.com>
2023-10-19 16:22:14 -0500

    Add files via upload

---
+++ Miguel alzate 19-10/data.js
@@ -0,0 +1,33 @@
+export  const data =
+[
+    {
+        "id":1,
+        "name": "BMW",
+        "image": "/img/1200.png"
+    },     {
+        "id":2,
+        "name":"bajaj",
+        "image": "/img/boxerS.png"
+    }, 
+    {
+        "id":3,
+        "name": "DR650",
+        "image": "/img/dr.png"
+    }, 
+    {
+        "id":4,
+        "name": "libero 125",
+        "image": "/img/libero.png"
+    }, 
+    {
+        "id":5,
+        "name": "rx115",
+        "image": "/img/rx.png"
+    }, 
+    {
+        "id":6,
+        "name": "xj6",
+        "image": "/img/xj6.png"
+    }, 
+
+]
